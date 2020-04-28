a28c42e743c8e1b2374ed5d000defaf69b5b6769
peterpan <apppeterpan@gmail.com>
2020-04-28 13:29:10 +0800

    Initial Commit

---
+++ IAPDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IAPDemo
+//
+//  Created by SHIH-YING PAN on 2020/4/28.
+//  Copyright © 2020 SHIH-YING PAN. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
